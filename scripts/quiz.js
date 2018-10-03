d91a0f183b01efe56845bb059097abb195af4169
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-03 06:27:13 +0300

    some changes in logical process

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,22 @@
-console.log('quiz.js loaded');
+//getting random number from 1 to 100
+let randomNumber = Math.floor(Math.random()*100)+1;
+console.log(randomNumber);
+let attempt = 0, maxAttempt = 10;
+let guessMsg = document.querySelector('.guessMsg');
+
+//add eventListener for button GetAnswer
+let getAnswerButton = document.getElementById('getAnswerButton');
+getAnswerButton.addEventListener('click', 
+	function (event) {
+		getUserNumber();
+}, false);
+
+function getUserNumber(){
+	attempt++;
+	let value = document.getElementById('userNumber').value;
+	value = Number(value);
+	if (attempt===1) {
+		guessMsg.textContent = 'Previous guesses:';
+	}
+	console.log(value);
+};
